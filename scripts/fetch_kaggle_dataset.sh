79d1fb527c8804c953374df5d340da5d4d0faa8e
Jaya Surya Rachabathuni <jaya.rachabathuni@solugenix.com>
2021-03-10 16:20:01 +0530

    kaggle

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-kaggle competitions download -c titanic -f test.csv -p packages/regression_model/regression_model/datasets/
+kaggle competitions download -c titanic -f test.csv -p packages/classification_model/classification_model/datasets/
  
-kaggle competitions download -c titanic -f train.csv -p packages/regression_model/regression_model/datasets/
+kaggle competitions download -c titanic -f train.csv -p packages/classification_model/classification_model/datasets/
